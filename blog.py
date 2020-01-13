03ee82936b6bc2acff6bab18f4caffbcb581f9c9
Benjamin J. Boyle <me@benboyle.ca>
2020-01-13 12:50:08 -0400

    large alterations to script

--- blog.py
+++ blog.py
@@ -1,16 +1,28 @@
 from pathlib import Path
 import os
 import shutil
+import datetime
+import calendar
+import sys
+import fileinput
+
+abbr_to_num = {name: num for num,
+               name in enumerate(calendar.month_abbr) if num}
+post_list = []
 
 # SITE
 md_dir = './md/'
 out_dir = './site/'
 res_out_dir = out_dir + 'res/'
+image_dir = './images/'
+image_out_dir = out_dir + image_dir[2:]
+netlify_file = './netlify.toml'
+netlify_out_file = out_dir + netlify_file[2:]
 
 # RES
 res_dir = './res/'
 pandoc_head = 'pandoc -s -t html5 -o '
-css_filename = 'github.css'
+css_filename = 'site.css'
 css_file = res_dir + css_filename
 css_out_file = res_out_dir + css_filename
 head_file = res_dir + 'head.html'
@@ -29,36 +41,46 @@ blog_index_file = md_dir + 'blog_index.md'
 blog_index_start = '% Blog Index'
 
 # POSTS
-pandoc_post = 'pandoc -s -t html5 -c %s -H %s -A %s -o ' % (css_file, head_file, comments_html)
+pandoc_post = 'pandoc -s -t html5 -c %s -H %s -A %s -o ' % (
+    css_file, head_file, comments_html)
 post_md_dir = md_dir + 'posts/'
 
+
 def clean_build():
-    quick_build()
+    delete_out_dir()
     build_blog_index()
+    quick_build()
+    copy_images()
+    copy_netlify()
+
 
 def quick_build():
-    delete_out_dir()
     build_res()
     build_base()
     build_posts()
+
 
 def delete_out_dir():
     if os.path.exists(out_dir):
         shutil.rmtree(out_dir)
 
+
 def build_res():
     build_head()
     copy_css()
 
+
 def build_head():
-    comd = '%s %s %s' % (pandoc_head, head_file, head_md_file)
     print('Building Header ...')
+    comd = '%s %s %s' % (pandoc_head, head_file, head_md_file)
     os.system(comd)
 
+
 def copy_css():
-    Path(res_out_dir).mkdir(parents=True, exist_ok=True)
     print('Copying CSS ...')
+    Path(res_out_dir).mkdir(parents=True, exist_ok=True)
     shutil.copyfile(src=res_dir + css_filename, dst=css_out_file)
+
 
 def build_base():
     if len(post_list) > 0:
@@ -70,6 +92,7 @@ def build_base():
         comd = '%s %s %s' % (pandoc_base, out_dir +
                              base_file[:-2] + 'html', base_md_dir + base_file)
         os.system(comd)
+
 
 def append_message_to_home():
     print('Appending Home Page Message ...')
@@ -83,7 +106,12 @@ def append_message_to_home():
 
     with open(homepage_file, 'w+') as file:
         file.write(filedata)
+
+
 def build_blog_index():
+    global post_list
+    print('Building Blog Index ...')
+    Path(out_dir).mkdir(parents=True, exist_ok=True)
     with open(blog_index_file, 'w+') as blog_index:
         blog_index.write(blog_index_start + '\n\n')
         posts = os.listdir(post_md_dir)
@@ -91,16 +119,52 @@ def build_blog_index():
             with open(post_md_dir + post, 'r') as post_file:
                 title = post_file.readline()[2:-1]
                 date = post_file.readline()[:-1]
-                blog_index.write('* [%s](%s) - %s \n' % (title, post[:-2] + 'html', date))
-    comd = '%s %s %s' % (pandoc_base, out_dir + 'blog_index.html', blog_index_file)
+                date = date.split()
+                date = datetime.date(
+                    int(date[2]), abbr_to_num[date[1]], int(date[0]))
+                post_list.append((post[:-2] + 'html', title, date))
+
+        post_list = sorted(post_list, key=lambda x: x[2], reverse=True)
+
+        for post in post_list:
+            date = post[2]
+            date = '%s %s %s' % (
+                str(date.day), calendar.month_name[date.month][:3], str(date.year))
+            blog_index.write('* [%s](%s) - %s \n' % (post[1], post[0], date))
+
+    comd = '%s %s %s' % (pandoc_base, out_dir +
+                         'blog_index.html', blog_index_file)
     os.system(comd)
 
+
 def build_posts():
+    print('Building Blog Posts ...')
     Path(out_dir).mkdir(parents=True, exist_ok=True)
     posts = os.listdir(post_md_dir)
     for post in posts:
-        comd = '%s %s %s' % (pandoc_post, out_dir + post[:-2] + 'html', post_md_dir + post)
+        comd = '%s %s %s' % (pandoc_post, out_dir +
+                             post[:-2] + 'html', post_md_dir + post)
         os.system(comd)
 
+
+def copy_images():
+    print('Copying Images ...')
+    Path(image_out_dir).mkdir(parents=True, exist_ok=True)
+    images = os.listdir(image_dir)
+    for image in images:
+        shutil.copyfile(src=image_dir + image, dst=image_out_dir + image)
+
+
+def copy_netlify():
+    print('Copying Netlify TOML ...')
+    Path(out_dir).mkdir(parents=True, exist_ok=True)
+    shutil.copyfile(src=netlify_file, dst=netlify_out_file)
+
+
 if __name__ == '__main__':
-    clean_build()
+    if len(sys.argv) > 1:
+        print('Clean Building ...')
+        clean_build()
+    else:
+        print('Quick Building ...')
+        quick_build()
