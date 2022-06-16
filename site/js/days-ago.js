3f047fb3a042c047fc318883fa7545a89ccd7355
GitHub Action(automated) <me+auto@benboyle.ca>
2022-06-16 18:52:51 +0000

    build site

---
+++ site/js/days-ago.js
@@ -0,0 +1,23 @@
+// Fuck JavaScript
+
+const DATE_STR_LEN = 10
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+// https://stackoverflow.com/a/15289883/7789162
+function dateDiffInDays(a, b) {
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+
+headers = document.getElementsByTagName("h3")
+for(let header of headers) {
+    let innerHTML = header.nextElementSibling.innerHTML
+    let postTitle = innerHTML.substring(0, innerHTML.length - DATE_STR_LEN)
+
+    let postDate = new Date(innerHTML.substring(innerHTML.length - DATE_STR_LEN))
+    let numDays = dateDiffInDays(postDate, new Date())
+
+    header.nextElementSibling.innerHTML = postTitle + numDays + " days ago"
+}
