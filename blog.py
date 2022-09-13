aa941aaae435e1d00472bcb7029f1683b837df0f
Ben Boyle <me@benboyle.ca>
2022-09-12 21:59:21 -0300

    move stateful/dynamic pages to ./tmp/ and dynamically add js scripts based on dict

--- blog.py
+++ blog.py
@@ -31,7 +31,7 @@ homepage_file = tmp_dir + "index.md"
 
 # POST INDEX
 post_index_md_name = "post-index.md"
-post_index_file = f"{md_dir}post-index.md"
+post_index_file = tmp_dir + post_index_md_name
 post_index_start = "% Post Index"
 post_type_order = ["T1", "T2", "RH", "MP", "T3"]
 post_type_dict = {
@@ -45,21 +45,18 @@ post_type_dict = {
     "MP": {"name": "Meta Post", "desc": "Posts about me and this site"},
     "T3": {"name": "Tier 3", "desc": "Why did I spend time writing this?"},
 }
-js_file = "./days-ago.js"
-js_out_dir = f"{out_dir}js/"
-js_out_file = js_out_dir + js_file[2:]
 
 # POSTS
 pandoc_post = f"pandoc {default_base} {default_content} {default_post} -o "
 post_md_dir = md_dir + "posts/"
 
+# JS
+js_dict = { homepage_file: [ "days-ago.js" ] }
 
 def clean_build():
     print("Clean Building ...")
-    delete_out_dir()
-    delete_temps()
+    mk_clean_dirs(stateful_dirs)
     build_head()
-    copy_css()
     build_posts(post_md_dir)
     build_blog_index(
         global_post_list_list,
@@ -68,18 +65,25 @@ def clean_build():
         post_index_file,
         post_index_start,
     )
+    generate_homepage()
     build_base()
     build_dynamic()
     copy_verbatim()
 
 
+def exec_pandoc(cmd, md_file):
+    # append js script tag(s) if page is supposed to have them
+    if md_file in js_dict.keys():
+        with open(md_file, "a+") as file:
+            for js_file in js_dict[md_file]:
+                file.write(f'\n\n<script type="text/javascript" src="js/{js_file}"></script>\n')
+    os.system(f"{cmd} {md_file}")
 
 
 def build_blog_index(
     post_list_list, post_md_dir, index_md_name, index_md_file, index_start
 ):
     print("Building Post Index ...")
-    Path(out_dir).mkdir(parents=True, exist_ok=True)
 
     post_dict = {key: [] for key in post_type_order}
 
@@ -126,73 +130,57 @@ def build_blog_index(
 
         index.write(output_str)
 
-    cmd = f"{pandoc_base} {out_dir}{index_md_name[:-2]}html {index_md_file}"
-    os.system(cmd)
-
-    for i, post_list in enumerate(global_post_list_list):
-        find_replace_latest_post_msg(post_list[0], f"#{i}#", post_type_order[i])
-
-    with open(homepage_file, "a+") as file:
-        file.write(f'\n\n<script type="text/javascript" src="js/{js_file}"></script>\n')
+    exec_pandoc(f"{pandoc_base} {out_dir}{index_md_name[:-2]}html", index_md_file)
 
 
-def find_replace_latest_post_msg(latest_post, to_replace, post_type):
-    print(f"Finding+Replacing Latest {post_type} Post Message ...")
-    source_file = base_homepage_file
-    if os.path.isfile(homepage_file):
-        source_file = homepage_file
-
-    with open(source_file, "r") as file:
+def generate_homepage():
+    # Homepage is dynamic so it doesn't get built as HTML until build_dynamic()
+    print(f"Generating Homepage ...")
+    filedata = ''
+    with open(base_homepage_file, "r") as file:
         filedata = file.read()
-
-    filedata = filedata.replace(
-        to_replace,
-        f"[{latest_post['title']}]({latest_post['path']}) - {latest_post['date']}",
-    )
+        for i, post_list in enumerate(global_post_list_list):
+            print(f"\tFinding+Replacing Latest {post_type_order[i]} Post Message ...")
+            filedata = filedata.replace(
+                f"#{i}#",
+                f"[{post_list[0]['title']}]({post_list[0]['path']}) - {post_list[0]['date']}",
+            )
 
     with open(homepage_file, "w+") as file:
         file.write(filedata)
 
 
-def delete_out_dir():
-    if os.path.exists(out_dir):
-        shutil.rmtree(out_dir)
-
-
-def delete_temps():
-    if os.path.exists(homepage_file):
-        os.remove(homepage_file)
-    if os.path.exists(post_index_file):
-        os.remove(post_index_file)
+def mk_clean_dirs(dirs):
+    for dir in dirs:
+        if os.path.exists(dir):
+            shutil.rmtree(dir)
+        Path(dir).mkdir(parents=True, exist_ok=False)
 
 
 def build_head():
     print("Building Header ...")
-    cmd = f"{pandoc_head} {head_file} {head_md_file}"
-    os.system(cmd)
-
-
-def copy_css():
-    print("Copying CSS ...")
-    Path(res_out_dir).mkdir(parents=True, exist_ok=True)
-    shutil.copyfile(src=res_dir + css_filename, dst=css_out_file)
+    exec_pandoc(f"{pandoc_head} {head_foot_dir}head.html", head_foot_dir + "head.md")
 
 
 def build_base():
     print("Building Base Files ...")
     base_files = os.listdir(base_md_dir)
     for base_file in base_files:
-        cmd = f"{pandoc_base} {out_dir + base_file[:-2]}html {base_md_dir + base_file}"
-        os.system(cmd)
+        exec_pandoc(f"{pandoc_base} {out_dir + base_file[:-2]}html", base_md_dir + base_file)
+
+
+def build_dynamic():
+    print("Building Dynamically Generated Files ...")
+    dyn_files = os.listdir(tmp_dir)
+    for dyn_file in dyn_files:
+        exec_pandoc(f"{pandoc_base} {out_dir + dyn_file[:-2]}html", tmp_dir + dyn_file)
 
 
 def build_posts(posts_md_dir):
     print("Building Posts ...")
-    Path(out_dir).mkdir(parents=True, exist_ok=True)
     posts = os.listdir(posts_md_dir)
     for post in posts:
-        cmd = f"{pandoc_post} {out_dir + post[:-2]}html {posts_md_dir + post}"
-        os.system(cmd)
+        exec_pandoc(f"{pandoc_post} {out_dir + post[:-2]}html", posts_md_dir + post)
 
 
 def copy_verbatim():
