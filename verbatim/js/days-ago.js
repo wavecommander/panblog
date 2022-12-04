4f6a03a9ee29ab7efa1669dac5f1a2ac10a92fb4
Ben Boyle <me@benboyle.ca>
2022-12-04 19:06:53 -0400

    change to local time instead of UTC for relative dates

--- verbatim/js/days-ago.js
+++ verbatim/js/days-ago.js
@@ -3,31 +3,23 @@
 const DATE_STR_LEN = 10;
 const _MS_PER_DAY = 1000 * 60 * 60 * 24;
 
-// https://stackoverflow.com/a/15289883/7789162
-function dateDiffInDays(a, b) {
-  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
-  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
-
-  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
-}
-
 headers = document.getElementsByTagName("h3")
-for(let header of headers) {
-    let innerHTML = header.nextElementSibling.innerHTML;
-    let postTitle = innerHTML.substring(0, innerHTML.length - DATE_STR_LEN);
+for (let header of headers) {
+  let innerHTML = header.nextElementSibling.innerHTML;
+  let postTitle = innerHTML.substring(0, innerHTML.length - DATE_STR_LEN);
 
-    let postDate = new Date(innerHTML.substring(innerHTML.length - DATE_STR_LEN));
-    let numDays = dateDiffInDays(postDate, new Date());
+  let postDate = new Date(innerHTML.substring(innerHTML.length - DATE_STR_LEN) + 'T00:00');
+  let numDays = Math.floor((new Date() - postDate) / _MS_PER_DAY);
 
-    let fullText = postTitle;
+  let fullText = postTitle;
 
-    if(numDays == 0) {
-      fullText += "today";
-    } else if(numDays == 1) {
-      fullText += "yesterday";
-    } else {
-      fullText += numDays + " days ago";
-    }
+  if (numDays == 0) {
+    fullText += "today";
+  } else if (numDays == 1) {
+    fullText += "yesterday";
+  } else {
+    fullText += numDays + " days ago";
+  }
 
-    header.nextElementSibling.innerHTML = fullText;
+  header.nextElementSibling.innerHTML = fullText;
 }
